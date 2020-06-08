29adc698b204f097dee817e32bb2357e98bea945
Vladimir <vladimirzq@Gmail.com>
2020-06-08 23:13:39 +0600

    linked list reverse realisation

--- linked-lists/script.js
+++ linked-lists/script.js
@@ -1,4 +1,5 @@
 // 1 --> 10 --> 5 --> 16
+// to insert between 10 and 5 -- 55;
 
 class LinkedList {
   constructor(value){
@@ -38,11 +39,12 @@ class LinkedList {
     return linkedListArr;
   }
   insert(index, value){  
+    const lastNodeIndex = this.length - 1;
     if(index === 0){
       return this.prepend(value);
-    } else if(index === this.length){
+    } else if(index === lastNodeIndex){
       return this.append(value);
-    } else if(index > this.length || index < 0){
+    } else if(index > lastNodeIndex || index < 0){
       console.log('wrong index');      
       return this;
     }
@@ -53,6 +55,7 @@ class LinkedList {
       next: nextNode
     }
     prevNode.next = nodeToInsert; 
+    this.length++;
     return this;
   }
   traverseToIndex(index){
@@ -64,12 +67,56 @@ class LinkedList {
     }
     return currentNode;
   }
+  remove(index){
+    const lastNodeIndex = this.length - 1;
+    if(index === 0){
+      this.head = this.head.next;
+      this.length--;
+      return this;
+    } else if(index === lastNodeIndex){
+      let lastNode = this.traverseToIndex(this.length - 2);
+      lastNode.next = null;      
+      this.length--;
+      return this;
+    } else if(index > lastNodeIndex || index < 0){
+      console.log('wrong index');      
+      return this;
+    }
+    let prevNode = this.traverseToIndex(index - 1);
+    let deletingNode = prevNode.next;
+    prevNode.next = deletingNode.next;
+    this.length--;
+    return this;
+  }
+  reverse(){
+    if(!this.head.next){
+      return this;
+    }
+    let tempOne = this.head;
+    this.tail = this.head;
+    let tempTwo = tempOne.next;
+    while(tempTwo){
+      const tempThree = tempTwo.next;
+      tempTwo.next = tempOne;
+      tempOne = tempTwo;
+      tempTwo = tempThree;
+    }
+    this.head.next = null;
+    this.head = tempOne;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
-console.log(myLinkedList.append(5));
-console.log(myLinkedList.append(16));
-console.log(myLinkedList.prepend(1));
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.append(235);
+myLinkedList.insert(4, 79);
 console.log(myLinkedList.insert(2, 55));
+console.log(myLinkedList.printList());
+// myLinkedList.remove(4);
+
+
+myLinkedList.reverse();
 
 console.log(myLinkedList.printList());
